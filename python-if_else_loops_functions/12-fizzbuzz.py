09a262bc98b46e948f957d5d4356fbb47d615ffd
mutaganzwa-desire <d.mutaganzwa-alustudent.com>
2025-03-06 00:32:55 -0800

    File created

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3:
+            print("Fizz", end = "")
+        elif i % 5:
+            print("Buzz", end = "")
+        elif i % 3 and i % 5:
+            print("FizzBuzz", end = "")
