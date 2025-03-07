8b2dac1ebb1e71a73b6d254f0109d19a069e19e1
mutaganzwa-desire <d.mutaganzwa-alustudent.com>
2025-03-07 12:00:48 -0800

    Files created

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class called Rectangle.
+such as width, height, and methods to calculate area, perimeter, etc.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle.
+
+    It currently has no attributes or methods. This class is a placeholder for
+    defining a rectangle. In future implementations, it can be extended with
+    """
+    pass
