700e98626c1098320d883ea2e165e0cfcc9a44b8
mutaganzwa-desire <d.mutaganzwa-alustudent.com>
2025-03-07 13:04:20 -0800

    LAst last

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(", ".join("{:d}{:d}".format(i, j) for i in range(10) for j in range(i + 1, 10)))
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=", " if i < 8 or j < 9 else "\n")
