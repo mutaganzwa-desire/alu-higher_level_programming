aa9994736a558ee916bca41055ff1080b5fa4250
mutaganzwa-desire <d.mutaganzwa-alustudent.com>
2025-03-07 11:09:52 -0800

    Files created

---
+++ 4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            if (i >= len(my_list_1)) or (i >= len(my_list_2)):
+                raise IndexError("out of range")
+            if (not isinstance(my_list_1[i], (int, float)) or
+               not isinstance(my_list_2[i], (int, float))):
+                raise TypeError("wrong type")
+            result.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        finally:
+            continue
+    return result
