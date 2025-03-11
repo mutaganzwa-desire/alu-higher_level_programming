66acdd5294869a2bc94204ea25f3b5730037e9e8
mutaganzwa-desire <d.mutaganzwa-alustudent.com>
2025-03-10 23:21:59 -0700

    File created

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,5 @@
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its content to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")  # end="" prevents extra newline
+
