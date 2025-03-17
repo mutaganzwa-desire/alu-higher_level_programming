f3e98fee99df5048333ea0c236c9a6063ac50510
mutaganzwa-desire <d.mutaganzwa-alustudent.com>
2025-03-16 22:16:23 -0700

    File created

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Task: List all privileges of MySQL users 'user_0d_1' and 'user_0d_2' on localhost
+
+-- Show privileges for user 'user_0d_1' on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user 'user_0d_2' on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
