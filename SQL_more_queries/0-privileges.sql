86ed159af502d41a30864c658a9fbdfcba05082b
mutaganzwa-desire <d.mutaganzwa-alustudent.com>
2025-04-05 07:59:26 -0700

    File updated

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
--- Task: List all privileges of MySQL users 'user_0d_1' and 'user_0d_2' on localhost
-
--- Show privileges for user 'user_0d_1' on localhost
+-- lists privileges of the MySQL users
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON *.* FROM 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show privileges for user 'user_0d_2' on localhost
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
