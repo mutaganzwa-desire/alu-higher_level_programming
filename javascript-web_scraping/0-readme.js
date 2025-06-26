6d4f3c399df2b36f04c1015691654b6767df3428
mutaganzwa-desire <d.mutaganzw@alustudent.com>
2025-06-26 10:06:16 +0200

    File created

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+// readFile.js
+
+const fs = require('fs');
+
+// Get the file path from command-line arguments
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.log('Usage: node readFile.js <file_path>');
+  process.exit(1);
+}
+
+// Read the file in utf-8 encoding
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
+
