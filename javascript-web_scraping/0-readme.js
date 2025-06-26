09ec77d920fa8e6ae1a9adc8d120396e411110d9
mutaganzwa-desire <d.mutaganzw@alustudent.com>
2025-06-26 10:22:32 +0200

    Altogether

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,23 +1,3 @@
 #!/usr/bin/node
-
-// readFile.js
-
 const fs = require('fs');
-
-// Get the file path from command-line arguments
-const filePath = process.argv[2];
-
-if (!filePath) {
-  console.log('Usage: node readFile.js <file_path>');
-  process.exit(1);
-}
-
-// Read the file in utf-8 encoding
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
-});
-
+fs.readFile(process.argv[2], 'utf-8', (err, data) => err ? console.log(err) : console.log(data));
